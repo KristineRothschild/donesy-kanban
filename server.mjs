ba171a72ec2da8921a62c081c7db91b4b0a9ad38
Kristine Sophia Rothschild <kristine.sophia95@gmail.com>
2026-01-18 22:14:40 +0100

    Scaffolded express and tested.

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
